acbc170d5a2d4fd4f05847bb285ab74c2e4a692c
Olga Coll <olgacoll94@gmail.com>
2024-12-12 18:06:53 +0100

    feat: initial structure of the project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Welcome to Moviedux</h1>
+      </header>
+      <footer clasName="footer">
+        <p className="footer">Footer content here</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
